077ef6fd1a1a7091e1e8edb486b9b55be6603411
Mukesh Anand <mukeshanandmax@gmail.com>
2024-05-25 17:36:29 +0530

    Time: 1 ms (99.47%), Space: 56.8 MB (63.36%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,6 +1,8 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         
+        //https://www.geeksforgeeks.org/batch/dsa-4/track/DSASP-Arrays/video/MTQwMg%3D%3D
+        
         int maxSum =Integer.MIN_VALUE;
         int maxSumEnding=0;
         int n = nums.length;
