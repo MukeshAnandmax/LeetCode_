2c8c4b6837f2b1514ac415337ba237c33ee33aa0
Mukesh Anand <mukeshanandmax@gmail.com>
2025-03-08 21:48:44 +0530

    Time: 23 ms (92.99%), Space: 47.1 MB (15.17%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,37 +1,20 @@
 class Solution {
-    public int subarraySum(int[] A, int B) {
-
-
-
-         HashMap<Long,Integer> hm = new HashMap<>();
-        int count=0;
-        long psum[] = new long[A.length];
-        psum[0]= A[0];
-      
-        for(int i=1;i<A.length;i++){
-
-            psum[i] =psum[i-1]+ A[i];
-         
-        }
-
-       
-
-         for(int i=0;i<A.length;i++){
-
-             if(psum[i] ==B){
-                 count++;
-             }
-
-            if(hm.containsKey(psum[i]-B)){
-
-                count += hm.get(psum[i]-B);
-                
-            }
-            hm.put(psum[i],hm.getOrDefault(psum[i],0)+1);
-
-         }
-
-        return count ;
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer,Integer> hm =  new HashMap<>();
+        int currentSum =0;
+        int count =0;
+        hm.put(0,1);
         
+        for(int i=0,n= nums.length;i<n;i++){
+            
+            currentSum+=nums[i];
+            
+            if(hm.containsKey(currentSum-k)){
+                count+=hm.get(currentSum-k);
+            }
+            hm.put(currentSum,hm.getOrDefault(currentSum,0)+1);
+        }
+        
+        return count;
     }
 }
